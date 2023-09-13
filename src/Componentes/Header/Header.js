a8c2854813a764545bd40e1943040fcf425f9612
Santiago Vargas <anglesla.santiagov@gmail.com>
2023-09-12 20:12:19 -0400

    Initial Folder Structure Set - up

---
+++ src/Componentes/Header/Header.js
@@ -0,0 +1,33 @@
+import "./Header.scss";
+import logoImage from "../../Assets/Images/ice-logo.png";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header className="header">
+      <div className="header__wrapper">
+        <div className="header-logo">
+          <a href="/">
+            <img className="header-logo__img" src={logoImage} alt="ice-logo" />
+          </a>
+        </div>
+        <nav className="nav">
+          <div className="nav-links">
+            <Link className="nav-links__page-link" to="#">
+              <button className="nav-links__current nav-links__current--active">
+                About
+              </button>
+            </Link>
+            <Link className="nav-links__page-link" to="#">
+              <button className="nav-links__inactive nav-links__inactive--hover">
+                Services
+              </button>
+            </Link>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
